002ee321afc2f2c875aa3a4fd7e490f6b7a743bb
alek <lyudskoy@gmail.com>
2024-06-25 22:50:51 +0500

    clarification

--- get-html.sh
+++ get-html.sh
@@ -1,4 +1,5 @@
-curl -v 'https://ru.vv3rd.tech' --connect-to 'vv3rd.tech:104.18.0.125' \
+WEGLOT_IP='104.18.0.125'
+curl -v 'https://ru.vv3rd.tech' --connect-to "vv3rd.tech:$WEGLOT_IP" \
 	-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0' \
 	-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' \
 	-H 'Accept-Language: en-US,en;q=0.5' \
