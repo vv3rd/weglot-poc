b7057de04004c7200fdd03a4f7f54cd1bc0e27eb
alek <lyudskoy@gmail.com>
2024-06-25 16:05:09 +0500

    deployable

--- api/main.ts
+++ api/main.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import path from 'node:path'
+import morgan from 'morgan'
 import { parseArgs, type ParseArgsConfig } from 'node:util'
 
 const __dirname = import.meta.dirname
@@ -15,8 +16,14 @@ const PORT = ARGS.values.port ?? 3003
 
 const app = express()
 
+app.use(morgan("short"))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.resolve(__dirname, "static")))
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`)
 })
+
+export default app
+
