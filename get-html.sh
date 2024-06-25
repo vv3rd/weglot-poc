27011b8286fa8a3d62d27fbd9ece184f46bcb51e
alek <lyudskoy@gmail.com>
2024-06-25 22:37:49 +0500

    working poc with curl

---
+++ get-html.sh
@@ -0,0 +1,10 @@
+curl -v 'https://ru.vv3rd.tech' --connect-to 'vv3rd.tech:104.18.0.125' \
+	-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0' \
+	-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' \
+	-H 'Accept-Language: en-US,en;q=0.5' \
+	-H 'Accept-Encoding: identity' \
+	-H 'Connection: keep-alive' \
+	-H 'Upgrade-Insecure-Requests: 1' \
+	-H 'Sec-Fetch-Dest: document' \
+	-H 'Sec-Fetch-Mode: navigate' \
+	-H 'Sec-Fetch-Site: cross-site'
